33dc71cb17073d0984a75a6f77323e4ca2f178e9
Yara <yaraezz151@gmail.com>
2022-05-09 15:36:43 +0200

    Footer Component

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../css/Footer/Footer.css";
+
+function Footer() {
+  return(
+    <footer>
+        &copy; Copyright Yara Mohamed 2022
+    </footer>
+  )
+}
+
+export default Footer;
