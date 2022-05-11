82807919eab9d42cfe53617ab5a0847accd97676
Yara <yaraezz151@gmail.com>
2022-05-11 03:02:05 +0200

    Product Details

--- client/src/components/Products/Products.jsx
+++ client/src/components/Products/Products.jsx
@@ -1,20 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "../../css/Products/Products.css"
-
+import ProductModal from '../../css/Products/ProductModal';
  function Products(props) {
      console.log(props);
+
+const [product, setProduct] = useState("");
+
+const openModal = (product) =>{
+    setProduct(product)
+}
+
+const closeModal = () =>{
+  setProduct(false)
+}
+
   return (
     <div className='products-wrapper'>
-    {props.products.map(Product => (
-    <div className="product-item" key={Product.id}>
-        <img src={Product.imageUrl} alt={Product.title} />
+    {props.products.map(product => (
+    <div className="product-item" key={product.id}>
+        <a href="#" onClick={() => openModal(product)}>
+        <img src={product.imageUrl} alt={product.title}  />
+        </a>
     <div className="product-desc">
-        <p>{Product.title}</p>
-        <span>{Product.price}</span>
+        <p>{product.title}</p>
+        <span>${product.price}</span>
     </div>
     <button>Add To Cart</button>
     </div>
 ))}
+
+<ProductModal product={product} closeModal={closeModal} />
+
+
 </div>
   )
 }
---
+++ client/src/css/Products/ProductModal.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Modal from "react-modal"
+
+ function ProductModal(props) {
+     const {product, closeModal} = props;
+
+  return (
+    
+<Modal isOpen={product} onRequestClose={closeModal}>
+  <span className='close-icon' onClick={closeModal}> X </span>
+<div className="product-info">
+<img src={product.imageUrl} alt={product.title} />
+<p>{product.title}</p>
+<p>${product.price}</p>
+</div>
+</Modal>
+  )
+}
+
+export default ProductModal
